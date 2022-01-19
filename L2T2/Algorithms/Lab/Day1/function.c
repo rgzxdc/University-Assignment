deedb4f21f90bd3e0f393f31b3858a061d6e2aec
Shayed Hasan <shayedhasanshakib@gmail.com>
2022-01-19 12:42:23 +0600

    halfway through the lab 1

---
+++ L2T2/Algorithms/Lab/Day1/function.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    
+    printf("Hello BLC!\n");
+    
+    int a, b, result;
+    scanf("%d %d", &a, &b);
+    result = a*b;
+    
+    printf("a X b is: %d", result);
+    return 0;
+}
