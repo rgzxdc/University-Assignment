579fe46ca0b14bbf0d899b2c1315c0b8517be12b
Shayed Hasan <shayedhasanshakib@gmail.com>
2022-08-01 13:50:28 +0600

    Lab Started

---
+++ L3T1/OOP_2_Lab/lab1.py
@@ -0,0 +1,60 @@
+x = 11
+y = 39
+print(x)
+print(x+10)
+print(x*x)
+x += 10
+print(x*y)
+print(x)
+
+
+
+a = input("Enter a Number: ")
+print(a)
+a = input("Enter your Name: ")
+print(a)
+
+
+
+A = (1,2,3,5,6)
+print(type(A))
+A = "Bangladesh"
+print(type(A))
+A = [1,2,3,4,"Country Road"]
+print(type(A))
+A = 102.65
+print(type(A))
+
+
+
+name = input("Enter Your Name: ")
+print(name)
+print(len(name))
+
+
+
+name = input("Enter Your Name: ")
+print(name)
+print(len(name))
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+print(name[6])
+print(name[7])
+print(name[8])
+print(name[9])
+print(name[10])
+print(name[11])
+print(name[12])
+
+
+
+name = input("Enter Your Name: ")
+print(name)
+print(len(name))
+print(name[-1])
+
+
